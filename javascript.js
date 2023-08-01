130db12397b664150152acbc2c994abc703021bd
Abdurahmon <abdurahmonmi6@gmail.com>
2023-08-01 09:16:12 -0700

    I have done top line

--- javascript.js
+++ javascript.js
@@ -1,3 +1,9 @@
-let body = document.body
-let removeSection = document.querySelector('section').style.height
-console.log(body.scrollHeight - removeSection);
+let body = document.body;
+let removeSection = document.querySelector('#a1');
+removeSection = window.getComputedStyle(removeSection).height
+let line = document.querySelector('.line')
+let bodyHeight = window.getComputedStyle(body).height
+
+addEventListener("scroll", function(e){
+    line.style.width = 100 / ((parseInt(bodyHeight) - parseInt(removeSection))  / this.window.scrollY) + "%"
+});
