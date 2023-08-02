6894c5f59b4f1189b45406f21fb05f6102aa2d6d
Abdurahmon <abdurahmonmi6@gmail.com>
2023-08-02 11:35:36 -0700

    text in drowers has doen

--- javascript.js
+++ javascript.js
@@ -1,9 +1,35 @@
 let body = document.body;
-let removeSection = document.querySelector('#a1');
+let removeSection = document.querySelector('section');
 removeSection = window.getComputedStyle(removeSection).height
 let line = document.querySelector('.line')
+let first = document.querySelector("#third");
 let bodyHeight = window.getComputedStyle(body).height
+let title = document.querySelectorAll('.title')
+
+
+
+// Step 2: Check if the element is visible
+
+
 
 addEventListener("scroll", function(e){
     line.style.width = 100 / ((parseInt(bodyHeight) - parseInt(removeSection))  / this.window.scrollY) + "%"
+    
 });
+
+title.forEach(element => {
+
+    element.addEventListener('click', ()=>{
+        console.log(element.nextElementSibling);
+        if(window.getComputedStyle(text).display == "block"){
+            text.style.display = "none"
+        }else{
+            text.style.display = "Block"
+        }
+        
+    })
+    
+});
+
+
+
